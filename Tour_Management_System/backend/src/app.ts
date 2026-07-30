c24bf58699821e109f16589b896ebef34ea36f65
Md. Motiur Rahman <motiur0883@gmail.com>
2026-07-30 12:46:00 +0600

    Add package routes to the application

--- Tour_Management_System/backend/src/app.ts
+++ Tour_Management_System/backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import packageRoutes from './routes/packageRoutes';
 
 const app: Application = express();
 
@@ -7,6 +8,9 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/v1/packages', packageRoutes);
 
 // Test Route
 app.get('/', (req: Request, res: Response) => {
